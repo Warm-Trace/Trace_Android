4c52cb8dd23215d3df3c51eb2cc7cda5417f16b3
choiminjun <mj010504@naver.com>
2025-08-09 01:18:46 +0900

    chore: 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     namespace = "com.virtuous.trace"
 
     defaultConfig {
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         targetSdk = 35
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
