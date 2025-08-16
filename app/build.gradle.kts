2b5ca586af46b4f412d9f713fba89f0c5e8556af
choiminjun <mj010504@naver.com>
2025-08-16 22:25:42 +0900

    release: 버전 코드 및 버전 이름 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     namespace = "com.virtuous.trace"
 
     defaultConfig {
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
         targetSdk = 35
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
