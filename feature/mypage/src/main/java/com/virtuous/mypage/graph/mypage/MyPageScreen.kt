88630c1787b54ff6d9b79913195cbabc3d6d7945
choiminjun <mj010504@naver.com>
2025-10-15 19:58:52 +0900

    refactor: Deprecated된 TabRow 수정

--- feature/mypage/src/main/java/com/virtuous/mypage/graph/mypage/MyPageScreen.kt
+++ feature/mypage/src/main/java/com/virtuous/mypage/graph/mypage/MyPageScreen.kt
@@ -1,6 +1,7 @@
 package com.virtuous.mypage.graph.mypage
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,9 +12,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
@@ -26,7 +30,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -240,31 +243,30 @@ private fun MyPageScreen(
 
                 Spacer(Modifier.height(28.dp))
 
-                TabRow(
-                    selectedTabIndex = tabs.indexOf(tabType),
-                    containerColor = Background,
-                    contentColor = Black,
-                    indicator = { tabPositions ->
+                SecondaryTabRow(
+                    tabs.indexOf(tabType),
+                    Modifier, Background,
+                    Black,
+                    {
                         TabRowDefaults.SecondaryIndicator(
-                            color = TabIndicator,
-                            modifier = Modifier
-                                .tabIndicatorOffset(tabPositions[tabs.indexOf(tabType)])
+                            Modifier.tabIndicatorOffset(tabs.indexOf(tabType))
                         )
-                    }
-                ) {
-                    tabs.forEach { tab ->
-                        Tab(
-                            selected = tab == tabType,
-                            onClick = { onTabTypeChange(tab) },
-                            text = {
-                                Text(
-                                    tab.label,
-                                    style = TraceTheme.typography.myPageTab
-                                )
-                            }
-                        )
-                    }
-                }
+                    },
+                    divider = @Composable { HorizontalDivider() },
+                    tabs = {
+                        tabs.forEach { tab ->
+                            Tab(
+                                selected = tab == tabType,
+                                onClick = { onTabTypeChange(tab) },
+                                text = {
+                                    Text(
+                                        tab.label,
+                                        style = TraceTheme.typography.myPageTab
+                                    )
+                                }
+                            )
+                        }
+                    })
 
                 Spacer(Modifier.height(20.dp))
             }
